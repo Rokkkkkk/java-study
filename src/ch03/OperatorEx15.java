1da8d113106053a03fad3f90fb4477234bad5153
Rokkkkkk <gxxx27@naver.com>
2022-03-26 20:01:56 +0900

    연산자 출력 예제 소스

---
+++ src/ch03/OperatorEx15.java
@@ -0,0 +1,9 @@
+package ch03;
+
+public class OperatorEx15 {
+    public static void main(String[] args) {
+        char upperCase = 'A';
+        char lowerCase =(char) (upperCase + 32);
+        System.out.println(lowerCase);
+    }
+}
