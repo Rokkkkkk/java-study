fe5b8eb9c37650eec10fca51a04841f9c48f3ec0
Rokkkkkk <gxxx27@naver.com>
2022-04-05 18:40:28 +0900

    반복(for, while, do-while, break, continue, 이름 붙은)문 출력 예제 소스

---
+++ src/ch04/while_example/FlowEx23.java
@@ -0,0 +1,11 @@
+package ch04.while_example;
+
+public class FlowEx23 {
+    public static void main(String[] args) {
+        int i= 5;
+
+        while(i--!=1) {
+            System.out.println(i + " - I can do it.");
+        }
+    }
+}
